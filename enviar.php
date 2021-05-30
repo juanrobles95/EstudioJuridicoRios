5ddf96b8e3e55eda5041e931a2245505d31ba612
juanrobles95 <juanrubenalexisrobles@gmail.com>
2021-05-30 19:58:09 -0300

    v3.99

--- enviar.php
+++ enviar.php
@@ -1,24 +1,24 @@
 <?php
-$name = $_POST['name'];
-$mail = $_POST['email'];
+$name     = $_POST['name'];
+$email    = $_POST['email'];
 $telefono = $_POST['telefono'];
-$mensaje = $_POST['mensaje'];
+$mensaje  = $_POST['mensaje'];
+$correo = "consultas@estudiojuridicorios.com.ar";
+$asunto = "FORM_SITIOWEB";
 
-$header = 'From: ' . $mail . " \r\n";
+
+$header  = 'From: ' . 'c2201267.ferozo.com' . " \r\n";
 $header .= "X-Mailer: PHP/" . phpversion() . " \r\n";
-$header .= "Mime-Version: 1.0 \r\n";
+$header .= "Mime-Version: 1.0" . "\r\n";
 $header .= "Content-Type: text/plain";
 
-$message = "Este mensaje fue enviado por:" . $name . "\r\n";
-$message .= "Su e-mail es: " . $mail . " \r\n";
+$message  = "CORREO: " . $email . " \r\n";
+$message .= "NOMBRE: " . $name . "\r\n";
 $message .= "Teléfono: " . $telefono . " \r\n";
-$message .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
-$message .= "Enviado el: " . date('d/m/Y', time());
+$message .= "Mensaje: " . $mensaje . " \r\n";
 
-$para = 'consultas@estudiojuridicorios.com.ar';
-$asunto = 'Mensaje enviado desde la pagina';
+//mb_send_mail($correo,$asunto,utf8_encode($mensaje),$header);
+mail($correo, $asunto, utf8_decode($message),$header);
 
-mail($para, $asunto, utf8_decode($message), $header);
-
-header("Location:mensaje_enviado.html");
+header("Location:mensaje_enviado.html")
 ?>
